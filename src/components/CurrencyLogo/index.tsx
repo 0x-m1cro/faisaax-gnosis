9a04214b2567756a48532121e1b3259c549a8789
0x-m1cro <0xm1cro@proton.me>
2023-04-26 00:47:10 +0500

    token list logos updated#2

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -1,8 +1,8 @@
-import { Currency, ETHER, Token } from '@uniswap/sdk'
+import { Currency, Token } from '@uniswap/sdk'
 import React, { useMemo } from 'react'
 import styled from 'styled-components'
 
-import EthereumLogo from '../../assets/images/ethereum-logo.png'
+//import EthereumLogo from '../../assets/images/ethereum-logo.png'
 import useHttpLocations from '../../hooks/useHttpLocations'
 import { WrappedTokenInfo } from '../../state/lists/hooks'
 import Logo from '../Logo'
@@ -10,12 +10,12 @@ import Logo from '../Logo'
 const getTokenLogoURL = () =>
   `https://github.com/faisaa-finance/faisaax-testnet/raw/master/public/images/core_logo.png`
 
-const StyledEthereumLogo = styled.img<{ size: string }>`
-  width: ${({ size }) => size};
-  height: ${({ size }) => size};
-  box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
-  border-radius: 24px;
-`
+// const StyledEthereumLogo = styled.img<{ size: string }>`
+//   width: ${({ size }) => size};
+//   height: ${({ size }) => size};
+//   box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
+//   border-radius: 24px;
+// `
 
 const StyledLogo = styled(Logo)<{ size: string }>`
   width: ${({ size }) => size};
@@ -34,7 +34,7 @@ export default function CurrencyLogo({
   const uriLocations = useHttpLocations(currency instanceof WrappedTokenInfo ? currency.logoURI : undefined)
 
   const srcs: string[] = useMemo(() => {
-    if (currency === ETHER) return []
+    //if (currency === ETHER) return []
 
     if (currency instanceof Token) {
       if (currency instanceof WrappedTokenInfo) {
@@ -46,9 +46,9 @@ export default function CurrencyLogo({
     return []
   }, [currency, uriLocations])
 
-  if (currency === ETHER) {
-    return <StyledEthereumLogo src={EthereumLogo} size={size} style={style} />
-  }
+  // if (currency === ETHER) {
+  //   return <StyledEthereumLogo src={EthereumLogo} size={size} style={style} />
+  // }
 
   return <StyledLogo size={size} srcs={srcs} alt={`token logo`} style={style} />
 }
